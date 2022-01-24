bb2ad2b6f51621eaf13849109dcd75ec4a1f6b61
annak <kalyuzh.ka@yandex.ru>
2022-01-24 19:38:30 +0300

    100%

--- database.py
+++ database.py
@@ -4,26 +4,53 @@ con = sqlite3.connect('results.sqlite')
 cur = con.cursor()
 cur.execute("""
 create table if not exists RECORDS (
-    name text,
-    score integer
-)
-""")
-
+    level text,
+    score integer    
+)""")
 cur.execute("""
-INSERT INTO records(name, score) VALUES('player', 5555)
-""")
+    INSERT INTO records(level, score) VALUES(1, 0)
+    """)
+cur.execute("""
+    INSERT INTO records(level, score) VALUES(2, 0)
+    """)
+cur.execute("""
+    INSERT INTO records(level, score) VALUES(3, 0)
+    """)
+
 con.commit()
 
+def insert_into(level, score):
+    con = sqlite3.connect('results.sqlite')
+    cur = con.cursor()
+    cur.execute("""
+    INSERT INTO records(level, score) VALUES({level}, {score})
+    """.format(level=level, score=score))
+    con.commit()
 
-result = cur.execute("""
-SELECT name, max(score) Score from Records
-GROUP by score
-ORDER by score DESC
-limit 3
-""").fetchall()
 
-for elem in result:
-    print(elem[1])
-print(result)
+def get_all_results():
+    con = sqlite3.connect('results.sqlite')
+    cur = con.cursor()
+    result = cur.execute("""
+        SELECT level, max(score) Score from Records
+        GROUP by score
+        ORDER by score DESC
+        limit 10
+        """).fetchall()
+    return result
+
+
+def get_result(level):
+    s = ''
+    con = sqlite3.connect('results.sqlite')
+    cur = con.cursor()
+    result = cur.execute("""
+    SELECT level, max(score) Score from Records
+    WHERE level = {n}
+    GROUP by score
+    ORDER by score DESC
+    limit 3
+    """.format(n=level)).fetchall()
+    return result[0][1]
 
 cur.close()
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import sys
 import random
 import pygame
+import sqlite3
+import database
+
 
 def print_text(text, x, y):
     font_color = (255, 255, 255)
@@ -39,19 +42,72 @@ class Button:
 
         print_text(text, x + 10, y + 10)
 
+
 def menu(a=None):
     menu = load_image('meanwhile.jpg')
     start_btn = Button(200, 50)
     exit_btn = Button(200, 50)
     running = True
     menu = pygame.transform.smoothscale(menu, screen.get_size())
+    Pause = False
+    vol = 1.0
     while running:
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    Pause = not Pause
+                    if Pause:
+                        pygame.mixer.music.pause()
+                    else:
+                        pygame.mixer.music.unpause()
+                elif event.key == pygame.K_MINUS:
+                    vol -= 0.1
+                    pygame.mixer.music.set_volume(vol)
+                elif event.key == pygame.K_PLUS:
+                    vol += 0.1
+                    pygame.mixer.music.set_volume(vol)
             if event.type == pygame.QUIT:
                 running = False
         screen.blit(menu, (0, 0))
         start_btn.draw(400, 20, 'Start game', choose_game)
         exit_btn.draw(400, 80, 'Exit game', exit)
+        pygame.display.flip()
+
+
+def results():
+    background = pygame.image.load('beach.jpg').convert()
+    background = pygame.transform.smoothscale(background, screen.get_size())
+    running = True
+    res = database.get_all_results()
+    back_to_menu_btn = Button(100, 50, (255, 127, 73), (255, 117, 56))
+    vol = 1.0
+    Pause = False
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    Pause = not Pause
+                    if Pause:
+                        pygame.mixer.music.pause()
+                    else:
+                        pygame.mixer.music.unpause()
+                elif event.key == pygame.K_MINUS:
+                    vol -= 0.1
+                    pygame.mixer.music.set_volume(vol)
+                elif event.key == pygame.K_PLUS:
+                    vol += 0.1
+                    pygame.mixer.music.set_volume(vol)
+            if event.type == pygame.QUIT or close_window:
+                running = False
+
+        screen.blit(background, (0, 0))
+        counter = 10
+        back_to_menu_btn.draw(800, 500, 'Back', choose_game, True)
+        for elem in res:
+            text3 = f'your score: {elem[1]} (for level {elem[0]})'
+            font = pygame.font.SysFont('Consolas', 20)
+            screen.blit(font.render(text3, True, (0, 0, 0)), (10, counter))
+            counter += 30
         pygame.display.flip()
 
 
@@ -66,8 +122,23 @@ def choose_game(a=None):
     level3_btn = Button(150, 150, (119, 221, 119), (80, 200, 120))
     back_to_menu_btn = Button(200, 50, (255, 160, 0), (237, 145, 33))
     results_table = Button(240, 50, (255, 160, 0), (237, 145, 33))
+    Pause = False
+    vol = 1.0
     while running:
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    Pause = not Pause
+                    if Pause:
+                        pygame.mixer.music.pause()
+                    else:
+                        pygame.mixer.music.unpause()
+                elif event.key == pygame.K_MINUS:
+                    vol -= 0.1
+                    pygame.mixer.music.set_volume(vol)
+                elif event.key == pygame.K_PLUS:
+                    vol += 0.1
+                    pygame.mixer.music.set_volume(vol)
             if event.type == pygame.QUIT or close_window:
                 running = False
         screen.blit(background, (0, 0))
@@ -77,9 +148,6 @@ def choose_game(a=None):
         results_table.draw(360, 540, 'Results table', results)
         back_to_menu_btn.draw(380, 600, 'Go to menu', menu, True)
         pygame.display.update()
-
-def results():
-    print('e')
 
 
 def play(level):
@@ -118,7 +186,6 @@ def play(level):
                     Cup.image = pygame.transform.flip(Cup.image, True, False)
                     self.rect.x -= 100
                     self.flag = 'left'
-
             self.rect.x += self.speedx
             if self.rect.right > width - 140 and self.flag == 'left':
                 self.rect.right = width - 140
@@ -128,6 +195,38 @@ def play(level):
                 self.rect.left = 40
             if self.rect.left < 140 and self.flag == 'right':
                 self.rect.left = 140
+
+    class Rednote(pygame.sprite.Sprite):
+        image = load_image("rednote.png")
+        image = pygame.transform.scale(image, (50, 50))
+
+        def __init__(self, group, y):
+            super().__init__(group)
+            self.image = Rednote.image
+            self.rect = self.image.get_rect()
+            self.mask = pygame.mask.from_surface(self.image)
+            self.rect.x = random.randint(15, width - 15)
+            self.rect.y = 0
+            self.collide = False
+            self.score = 0
+
+        def update(self, *args, level):
+            self.level = level
+            if level == 1:
+                self.rect = self.rect.move(0, 2)
+            if level == 2:
+                self.rect = self.rect.move(0, 3)
+            if level == 3:
+                self.rect = self.rect.move(0, 5)
+            if self.rect.bottom > height:
+                self.kill()
+            if pygame.sprite.collide_mask(self, cup):
+                self.collide = True
+            else:
+                self.collide = False
+            if self.collide:
+                Notes.catches += 15
+                self.kill()
 
     class Gorillaz(pygame.sprite.Sprite):
         image = load_image('gorillaz.png')
@@ -171,7 +270,6 @@ def play(level):
         image_boom = pygame.transform.scale(image_boom, (50, 50))
         catches = 0
 
-
         def __init__(self, group, y):
             super().__init__(group)
             self.image = Notes.image
@@ -181,6 +279,7 @@ def play(level):
             self.rect.y = y
             self.collide = False
             self.score = 0
+            self.bomb = False
 
         def update(self, *args, level):
             self.level = level
@@ -195,10 +294,14 @@ def play(level):
             if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
                     self.rect.collidepoint(args[0].pos):
                 self.image = self.image_boom
+                self.bomb = True
             if pygame.sprite.collide_mask(self, cup):
                 self.collide = True
             else:
                 self.collide = False
+            if self.collide and self.bomb:
+                Notes.catches += 10
+                self.kill()
             if self.collide:
                 Notes.catches += 1
                 self.kill()
@@ -219,36 +322,67 @@ def play(level):
     pygame.time.set_timer(MYEVENTTYPE, 1000)
     font = pygame.font.SysFont("Consolas", 20)
     time = 20
-
+    t = random.randrange(time)
+    vol = 1.0
+    Pause = False
+    GamePause = False
     while running:
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    Pause = not Pause
+                    if Pause:
+                        pygame.mixer.music.pause()
+                    else:
+                        pygame.mixer.music.unpause()
+                elif event.key == pygame.K_p and not GamePause:
+                    GamePause = True
+
+                elif event.key == pygame.K_p and GamePause:
+                    GamePause = False
+                elif event.key == pygame.K_MINUS:
+                    vol -= 0.1
+                    pygame.mixer.music.set_volume(vol)
+                elif event.key == pygame.K_PLUS:
+                    vol += 0.1
+                    pygame.mixer.music.set_volume(vol)
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                for bomb in all_sprites:
-                    all_sprites.update(event, level)
+                if not GamePause:
+                    for bomb in all_sprites:
+                        all_sprites.update(event, level=level)
             if event.type == MYEVENTTYPE:
-                m = Notes(all_sprites, 0)
-                notes.add(m)
-                time -= 1
+                if not GamePause:
+                    m = Notes(all_sprites, 0)
+                    notes.add(m)
+                    time -= 1
+            if time == t:
+                rednote = Rednote(all_sprites, 0)
             if time == 0:
-                game_over()
+                game_over(level)
                 running = False
-
-        one_sprite.update()
+        if not GamePause:
+            one_sprite.update()
         screen.blit(background, (0, 0))
         clock.tick(fps)
         all_sprites.draw(screen)
         one_sprite.draw(screen)
-        all_sprites.update(level=level)
+        if not GamePause:
+            all_sprites.update(level=level)
+        if GamePause:
+            text3 = f'press "p" to continue'
+            font = pygame.font.SysFont('Consolas', 80)
+            screen.blit(font.render(text3, True, (0, 0, 0)), (50, 300))
         text = f'score:{str(Notes.catches)}'
         text2 = f'time:{str(time)}'
+        text3 = f'ur best score: {database.get_result(level)}'
         font = pygame.font.SysFont('Consolas', 20)
+        screen.blit(font.render(text3, True, (0, 0, 0)), (10, 10))
         screen.blit(font.render(text2, True, (0, 0, 0)), (900, 30))
         screen.blit(font.render(text, True, (0, 0, 0)), (900, 10))
         returning = Notes.catches
         pygame.display.flip()
-
 
 
 def load_image(name, colorkey=None):
@@ -274,8 +408,24 @@ def start_game(level):
     counter, text = 10, '10'.center(16)
     pygame.time.set_timer(pygame.USEREVENT, 1000)
     font = pygame.font.SysFont('Consolas', 100)
+    skip_btn = Button(100, 50, (255, 63, 5), (209, 49, 0))
+    flPause = False
+    vol = 1.0
     while True:
         for e in pygame.event.get():
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_SPACE:
+                    flPause = not flPause
+                    if flPause:
+                        pygame.mixer.music.pause()
+                    else:
+                        pygame.mixer.music.unpause()
+                elif e.key == pygame.K_MINUS:
+                    vol -= 0.1
+                    pygame.mixer.music.set_volume(vol)
+                elif e.key == pygame.K_PLUS:
+                    vol += 0.1
+                    pygame.mixer.music.set_volume(vol)
             if e.type == pygame.USEREVENT:
                 counter -= 1
                 if counter > 0:
@@ -288,6 +438,7 @@ def start_game(level):
                 break
         else:
             screen.blit(background, (0, 0))
+            skip_btn.draw(450, 500, 'Skip', play, True, l=level)
             screen.blit(font.render(text, True, (0, 0, 0)), (25, 48))
             pygame.display.flip()
             clock.tick(60)
@@ -295,32 +446,52 @@ def start_game(level):
         break
     play(level)
 
-def game_over(a=None):
+
+def game_over(level):
+    level = level
     end = load_image('gameover.jpg')
     running = True
     close_window = False
     end = pygame.transform.smoothscale(end, screen.get_size())
     gotomenu_btn = Button(200, 50)
+    score = returning
+    database.insert_into(level, returning)
+    vol = 1.0
+    Pause = False
     while running:
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    Pause = not Pause
+                    if Pause:
+                        pygame.mixer.music.pause()
+                    else:
+                        pygame.mixer.music.unpause()
+                elif event.key == pygame.K_MINUS:
+                    vol -= 0.1
+                    pygame.mixer.music.set_volume(vol)
+                elif event.key == pygame.K_PLUS:
+                    vol += 0.1
+                    pygame.mixer.music.set_volume(vol)
             if event.type == pygame.QUIT or close_window:
                 running = False
         screen.blit(end, (0, 0))
-        gotomenu_btn.draw(400, 500, 'Exit',  menu, True)
-        score = returning
+        gotomenu_btn.draw(400, 500, 'Exit', menu, True)
+
         font = pygame.font.SysFont('Consolas', 100)
         screen.blit(font.render(str(score), True, (0, 0, 0)), (660, 300))
         pygame.display.update()
-    print(returning)
 
 
 if __name__ == '__main__':
     pygame.init()
+    pygame.mixer.music.load("{x}.mp3".format(x=random.randint(0, 6)))
+    pygame.mixer.music.play()
+    for i in range(7):
+        pygame.mixer.music.queue("{i}.mp3".format(i=i))
     size = width, height = 1000, 700
     screen = pygame.display.set_mode(size, flags=pygame.NOFRAME)
     clock = pygame.time.Clock()
     pygame.display.flip()
     menu()
     pygame.quit()
-
-
